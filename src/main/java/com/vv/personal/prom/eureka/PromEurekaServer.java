7e50187ebbd077f417e588643f957427619b9325
Vivek <vivektheintel@gmail.com>
2020-12-22 23:43:58 +0530

    Adding beans for swagger and proto end points

--- src/main/java/com/vv/personal/prom/eureka/PromEurekaServer.java
+++ src/main/java/com/vv/personal/prom/eureka/PromEurekaServer.java
@@ -3,6 +3,11 @@ package com.vv.personal.prom.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -12,4 +17,16 @@ public class PromEurekaServer {
         SpringApplication.run(PromEurekaServer.class, args);
     }
 
+    @Bean
+    ProtobufHttpMessageConverter protobufHttpMessageConverter() {
+        return new ProtobufHttpMessageConverter();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.vv.personal.prom.eureka"))
+                .build();
+    }
 }
