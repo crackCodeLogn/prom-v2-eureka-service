78fa625c381caac925eddc61811728bb5923c957
Vivek <vivektheintel@gmail.com>
2020-12-27 00:25:56 +0530

    Readying for Heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/prom-eureka-service-0.0.1-SNAPSHOT.jar
--- src/main/java/com/vv/personal/prom/eureka/PromEurekaServer.java
+++ src/main/java/com/vv/personal/prom/eureka/PromEurekaServer.java
@@ -1,19 +1,31 @@
 package com.vv.personal.prom.eureka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
 import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import static com.vv.personal.prom.eureka.constants.Constants.*;
+
 @SpringBootApplication
 @EnableEurekaServer
 @EnableFeignClients
 public class PromEurekaServer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PromEurekaServer.class);
 
     public static void main(String[] args) {
         SpringApplication.run(PromEurekaServer.class, args);
@@ -30,5 +42,26 @@ public class PromEurekaServer {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.vv.personal.prom.eureka"))
                 .build();
+    }
+
+    @Autowired
+    private Environment environment;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void firedUpAllCylinders() {
+        String host = LOCALHOST;
+        try {
+            host = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            LOGGER.error("Failed to obtain ip address. ", e);
+        }
+        String port = environment.getProperty(LOCAL_SPRING_PORT);
+        String herokuHost = environment.getProperty(SPRING_APPLICATION_HEROKU);
+        LOGGER.info("Eureka service is up now! Expected Heroku Swagger running on: {}, exact url: {}",
+                String.format(HEROKU_SWAGGER_UI_URL, herokuHost),
+                String.format(SWAGGER_UI_URL, host, port));
+        LOGGER.info("Expected Heroku eureka page running on: {}, exact url: {}",
+                String.format(HEROKU_HOST_URL, herokuHost),
+                String.format(HOST_URL, host, port));
     }
 }
