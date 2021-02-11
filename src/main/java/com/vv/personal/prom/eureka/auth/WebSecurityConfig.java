ea177c5da4062591e819f7e40e5a25911bb7fa7b
Vivek <vivektheintel@gmail.com>
2021-02-11 09:39:52 +0530

    Incorporating basic auth for eureka

---
+++ src/main/java/com/vv/personal/prom/eureka/auth/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.vv.personal.prom.eureka.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author Vivek
+ * @since 11/02/21
+ */
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+
+}
